c68dfa479e739ee67fbe9df851cdbba0472509c7
R-Prince <reneprince93@icloud.com>
2021-05-16 21:19:15 +0000

    Add postgresql database and make default db

--- scrumble/settings.py
+++ scrumble/settings.py
@@ -95,8 +95,12 @@ WSGI_APPLICATION = 'scrumble.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rmsgagdw',
+        'USER': 'rmsgagdw',
+        'PASSWORD': 'K-aCgWwrC9e9E_JjzOmagtTImvUQmjpp',
+        'HOST': 'tai.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
 
